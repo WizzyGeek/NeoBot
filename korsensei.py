cf6195008651eff6c54ba8f9d01fc03a7a0cc24b
TEEN-BOOM <51919967+TEEN-BOOM@users.noreply.github.com>
2019-09-15 12:16:38 +0530

    Add files via upload

---
+++ korsensei.py
@@ -0,0 +1,43 @@
+import os
+import discord
+from discord.ext import commands
+import random
+#----------------------------------------#
+client = commands.Bot(command_prefix="$")
+guild = discord.Guild
+#----------------------------------------#
+@client.event
+async def on_ready():
+    await client.change_presence(activity=discord.Activity(name='my students', type=discord.ActivityType.watching, status=discord.Status.idle))
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(f'Hi {member.name}, welcome to my Discord server!')
+    channel = client.get_channel(583703372725747713)
+    await channel.send("welcome to the gang {member.mention}")
+
+@client.command()
+async def load(ctx, extension):
+    client.load_extension(f"cogs.{extension}")
+    await ctx.send("loaded!")
+
+@client.command()
+async def reload(ctx, extension):
+    client.unload_extension(f"cogs.{extension}")
+    client.load_extension(f"cogs.{extension}")
+    await ctx.send("reloaded!")
+
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f"cogs.{extension}")
+    await ctx.send("unloaded!")
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
+    else:
+        pass
+
+client.run('NjE5ODk2OTcyMTUyOTMwMzA4.XXdlXA.eDzLIWfhfgwArT93-JwCzbc0np0')
