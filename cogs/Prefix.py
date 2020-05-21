1217a4153c5661662556ff1bb6782cc167405b23
TEEN-BOOM <ojasscoding@gmail.com>
2020-05-21 12:27:48 +0530

    prefix

---
+++ cogs/Prefix.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+
+from bot import korosensei
+
+class Prefix(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.prefixes = korosensei.prefixes
+        
+    @commands.command(name="prefix")
+    async def prefix(self, ctx, prefix=None):
+        main_prefix = self.prefixes[ctx.guild.id][0]
+        if prefix is None:
+            await ctx.send(f"The prefix is {main_prefix}")
+        else:
+            await ctx.send("-------Under construction!-------")
+        
+def setup(bot):
+    bot.add_cog(Prefix(bot))
