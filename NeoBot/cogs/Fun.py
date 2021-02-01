2b3652ea3545236fd6b2974f448d64b48c6c5464
WizzyGeek <51919967+WizzyGeek@users.noreply.github.com>
2021-02-01 11:09:03 +0530

    clean up

--- NeoBot/cogs/Fun.py
+++ NeoBot/cogs/Fun.py
@@ -180,28 +180,7 @@ class Fun(commands.Cog):
 
     @commands.command(aliases=['8ball', '8b', "luckyball"])
     async def _bball(self, ctx):
-        ans = ['● It is certain.',
-               '● It is decidedly so.',
-               '● Without a doubt.',
-               '● Yes - definitely.',
-               '● You may rely on it.',
-               '● As I see it, yes.',
-               '● Most likely.',
-               '● Outlook good.',
-               '● Yes.',
-               '● Signs point to yes.',
-               '● Reply hazy, try again.',
-               '● Ask again later.',
-               '● Better not tell you now.',
-               '● Cannot predict now.',
-               '● Concentrate and ask again.',
-               '● Don\'t count on it.',
-               '● My reply is no.',
-               '● My sources say no.',
-               '● Outlook not so good.',
-               '● Very doubtful.'
-               ]
-        await ctx.send(embed=self.bot.Qembed(ctx, content=random.choice(ans)))
+        await ctx.send(embed=self.bot.Qembed(ctx, content=random.choice(self.ball)))
 
     #----------------------------------------#
     
