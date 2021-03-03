24d2598e13d7bc608e713dabac6d07892f1076c2
WizzyGeek <51919967+WizzyGeek@users.noreply.github.com>
2021-03-03 15:26:48 +0530

    Enable members intent

--- __main__.py
+++ __main__.py
@@ -1,6 +1,12 @@
+from discord import Intents
 from NeoBot import Neo, Config
+
+def get_intents() -> Intents:
+    intents = Intents.all()
+    intents.typing = False
+    return intents
 
 if __name__ == "__main__":
     ConfigObj: Config = Config()
-    NeoBot: Neo = Neo(ConfigObj, description="**Assassination\'s discord bot**\ndeveloped by TEEN_BOOM [here!](https://github.com/TEEN-BOOM/NeoBot) with discord.py")
+    NeoBot: Neo = Neo(ConfigObj, description="**Assassination\'s discord bot**\ndeveloped by TEEN_BOOM [here!](https://github.com/TEEN-BOOM/NeoBot) with discord.py", intents=get_intents())
     NeoBot.run()
